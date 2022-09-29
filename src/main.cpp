777851b2f06081ee8481e4c0b4789398aec3e536
Marvin Ahl <marvin_ahl@t-online.de>
2022-09-29 17:11:02 +0200

    SBUS works now

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,32 @@
 #include <SBUS2.h>
 #include <ESP32Servo.h>
 
-void setup() {
-    
+uint8_t sbus_fer = 0;
+int16_t sbus_channels[18] = {0};
+
+void setup()
+{
+    Serial.begin(115200);
+
+    Serial.println("Setup SBUS ...");
+    SBUS2_Setup();
 }
 
-void loop() {
+void loop()
+{
+    if (SBUS_Ready()) {
+        for (uint8_t i = 0; i < 18; ++i) {
+            sbus_channels[i] = SBUS2_get_servo_data(i);
+        }
+        sbus_fer = SBUS_get_FER();  // Frame Error Rate
+    }
 
+    for (uint8_t i = 0; i < 18; ++i) {
+        Serial.print(sbus_channels[i]);
+        Serial.print(" ");
+    }
+    Serial.print(sbus_fer);
+    Serial.println();
+
+    delay(50);
 }
